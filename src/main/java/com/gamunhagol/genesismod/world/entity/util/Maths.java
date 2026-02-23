faedc02e3802bec4d20b1bc3c878d88956999756
fran1 <fran1203ss@gmail.com>
2026-02-23 21:54:01 +0900

    .

---
+++ src/main/java/com/gamunhagol/genesismod/world/entity/util/Maths.java
@@ -0,0 +1,11 @@
+package com.gamunhagol.genesismod.world.entity.util;
+
+public class Maths {
+    public static float rad(float deg) {
+        return (float) Math.toRadians(deg);
+    }
+
+    public static float rad(double deg) {
+        return (float) Math.toRadians(deg);
+    }
+}
