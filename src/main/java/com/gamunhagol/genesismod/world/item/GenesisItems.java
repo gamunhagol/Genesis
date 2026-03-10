6a4e712313ef6c0903e9230b94f3a2ada904bb60
fran1 <fran1203ss@gmail.com>
2026-03-10 14:30:43 +0900

    .

--- src/main/java/com/gamunhagol/genesismod/world/item/GenesisItems.java
+++ src/main/java/com/gamunhagol/genesismod/world/item/GenesisItems.java
@@ -378,7 +378,7 @@ public class GenesisItems {
     public static final RegistryObject<Item> PEWRIESE_PLATE_BOOTS = ITEMS.register("pewriese_plate_boots", () -> new PewriesePlateArmor(GenesisArmorMaterials.PEWRIESE_PLATE,
             ArmorItem.Type.BOOTS,new Item.Properties().fireResistant()));
 
-    public static final RegistryObject<Item> HOLY_KNIGHT_HELMET = ITEMS.register("holy_knight_helmet", () -> new BailiffArmor(GenesisArmorMaterials.HOLY_KNIGHT,
+    public static final RegistryObject<Item> HOLY_KNIGHT_HELMET = ITEMS.register("holy_knight_helmet", () -> new HolyKnightArmor(GenesisArmorMaterials.HOLY_KNIGHT,
             ArmorItem.Type.HELMET,new Item.Properties().fireResistant().rarity(Rarity.UNCOMMON)));
     public static final RegistryObject<Item> HOLY_KNIGHT_CHESTPLATE = ITEMS.register("holy_knight_chestplate", () -> new HolyKnightArmor(GenesisArmorMaterials.HOLY_KNIGHT,
             ArmorItem.Type.CHESTPLATE,new Item.Properties().fireResistant().rarity(Rarity.UNCOMMON)));
